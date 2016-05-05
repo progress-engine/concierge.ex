696450f83ae64924b99705f99a9a4a32c207a450
Ivan Kryak <kryak.iv@gmail.com>
2016-05-05 14:29:24 +0500

    Adjust Confirmable specs

--- extensions/confirmable/test/concierge/resource_test.exs
+++ extensions/confirmable/test/concierge/resource_test.exs
@@ -27,4 +27,33 @@ defmodule Concierge.ResourceTest do
       assert called Mailgun.Client.send_email(:_, :_)
     end
   end
+
+  test "it refuses sign in if user unconfirmed" do
+     {:ok, user = %Confirmable.TestUser{}} = Concierge.Resource.Registration.create(%{
+      "email" => "concierge@test.com", 
+      "password" => "123456789", 
+      "password_confirmation" => "123456789",
+    })
+
+    refute Confirmable.Resource.confirmed?(user)
+
+    assert {:error, _} = Concierge.Resource.Session.sign_in(Phoenix.ConnTest.conn(), user)
+  end
+
+  test "it signs in confirmed user" do
+    {:ok, user = %Confirmable.TestUser{}} = Concierge.Resource.Registration.create(%{
+      "email" => "concierge@test.com", 
+      "password" => "123456789", 
+      "password_confirmation" => "123456789",
+    })
+    {:ok, user} = Confirmable.Resource.confirm!(user)
+
+    assert Confirmable.Resource.confirmed?(user)
+
+    with_mock Guardian.Plug, [sign_in: fn conn, _ -> conn end] do
+      assert {:ok, %Plug.Conn{}} = Concierge.Resource.Session.sign_in(Phoenix.ConnTest.conn(), user)
+
+      assert called Guardian.Plug.sign_in(:_, user)
+    end
+  end
 end
