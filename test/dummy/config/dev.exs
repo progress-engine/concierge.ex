83282b2a693c6daccf8a5085d3110db53ea38c7f
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2016-05-05 12:52:45 +0300

    rollback of credentials

--- test/dummy/config/dev.exs
+++ test/dummy/config/dev.exs
@@ -36,8 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :dummy, Dummy.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "",
+  password: "",
   database: "dummy_dev",
   hostname: "localhost",
   pool_size: 10
